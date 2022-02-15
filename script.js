c9188ab9dbb92db3c91365eafd238224b61b6a63
Bruno Da Silva <99687674+3runoSilva@users.noreply.github.com>
2022-02-15 11:34:39 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//USEFUL URLS USED IN THIS PROJECT
+
+//Window.localStorage: https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+// Used localStorage to save information in the browser to make sure that the selected theme saves even after page refresh.
+let theme = localStorage.getItem("theme");
+
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "default.css";
+  }
+
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
+
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
+
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+
+  localStorage.setItem("theme", mode);
+}
+
+const foo = [1, 2, 3];
+const [n] = foo;
+console.log(n);
