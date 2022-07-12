03920c88de189409f4d87357fc8ce35e7776db17
Bruno Da Silva <99687674+3runoSilva@users.noreply.github.com>
2022-07-12 12:11:54 +0200

    commit to fix resume
    this commit serves at reverting the resume to a point of good stability and reverting a lot of new function that were implemented but not finished or tested.

--- script.js
+++ script.js
@@ -22,12 +22,62 @@ for (var i = 0; themeDots.length > i; i++) {
 
 function setTheme(mode) {
   if (mode == "light") {
-    document.getElementById("theme-style").href = "/styles/default.css";
+    document.getElementById("theme-style").href = "default.css";
   }
 
   if (mode == "blue") {
-    document.getElementById("theme-style").href = "/styles/blue.css";
+    document.getElementById("theme-style").href = "blue.css";
+  }
+
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
+
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
   }
 
   localStorage.setItem("theme", mode);
+}
+
+const foo = [1, 2, 3];
+const [n] = foo;
+console.log(n);
+
+$(function () {
+  "use strict";
+
+  // Give Active Class
+
+  $(".major").first().addClass("active");
+
+  // Show class"major"
+
+  $(".minor").first().show().animate({ width: "40%" });
+
+  // Show class"minor" On Click
+
+  $(".major").click(function () {
+    $(this).addClass("active").siblings(".major").removeClass("active");
+
+    $(this)
+      .next()
+      .show()
+      .animate({ width: "40%" })
+      .siblings(".minor")
+      .animate({ width: 0 }, function () {
+        $(this).hide();
+      });
+  });
+});
+
+let container = $("div");
+
+function competencees() {
+  let scrollTo = $(".expertise");
+  let position =
+    scrollTo.offset().top - container.offset().top + container.scrollTop();
+  container.animate({
+    scrollTop: position,
+  });
 }
